dfdf4960a9852236b7c0948f8e7f4467445fb7c4
Jackie First <jackiejeep@gmail.com>
2018-04-12 23:42:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(n)
-  if (n % 3 == 0 && n % 5 == 0)
+  if n % 3 == 0 && n % 5 == 0
     return "FizzBuzz"
-  else if (n % 3 == 0) 
+  else if n % 3 == 0 
     return "Fizz"
-  else if (n % 5 == 0)
+  else if n % 5 == 0
     return "Buzz"
   else 
     return n 
